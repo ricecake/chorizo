c5c766518b3b19695d93daab6c90c1a6593b7196
Sebastian Green-Husted <ricecake@tfm.nu>
2016-12-08 09:03:33 -0500

    Update setup.sql

--- SQL/setup.sql
+++ SQL/setup.sql
@@ -1,11 +1,11 @@
 BEGIN;
-Create table users
-CREATE TABLE workgroup
-Create table tasks
-Create table workgroup_users
-Create table intervals
-Create table task_intervals
-Create table task_options
-Create table task_occurence
-Create table task_claim
+Create table users ();
+CREATE TABLE workgroup ();
+Create table tasks ();
+Create table workgroup_users ();
+Create table intervals ();
+Create table task_intervals ();
+Create table task_options ();
+Create table task_occurence ();
+Create table task_claim ();
 COMMIT;
